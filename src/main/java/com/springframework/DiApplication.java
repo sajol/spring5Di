122828202b195a1b55e58a8f98d2871651a5c5d0
sajol <sajolkk@gmail.com>
2017-12-26 19:17:24 +0600

    get and use controller from application context

--- src/main/java/com/springframework/DiApplication.java
+++ src/main/java/com/springframework/DiApplication.java
@@ -1,12 +1,16 @@
 package com.springframework;
 
+import com.springframework.controllers.TestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(DiApplication.class, args);
+        TestController testController = (TestController) context.getBean("testController");
+        testController.sayHello();
+    }
 }
