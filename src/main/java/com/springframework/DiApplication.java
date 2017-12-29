88b1246084c7e865d9697b2baef87b7edea51eef
sajol <sajolkk@gmail.com>
2017-12-29 19:43:47 +0600

    component scanning with basePackages(override default behavior)

--- src/main/java/com/springframework/DiApplication.java
+++ src/main/java/com/springframework/DiApplication.java
@@ -7,8 +7,10 @@ import com.springframework.controllers.TestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.service", "com.springframework"})
 public class DiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/springframework/service/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package com.springframework.service;
-
-/**
- * Author: sazal
- * Date: 12/26/17.
- */
-public interface GreetingService {
-    String sayHello();
-}
